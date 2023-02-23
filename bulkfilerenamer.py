8de36fbb674e617dfee52b9a73cc287389d7d17e
IKIGA-I <golamrahman268224@gmail.com>
2023-02-23 10:23:57 +0800

    Updated the input system, user only needs to copy paste file path now

--- bulkfilerenamer.py
+++ bulkfilerenamer.py
@@ -1,15 +1,16 @@
 import os #letting the program know what os we are using currently
 
-pathinput = input('Input file path[After C:/Users/golam/](Please replace backslashes with forward slashes): ')
-
+pathinputtemp = input('Input file path: ')
+                                                #Taking input of File path can be modified
+pathinput = pathinputtemp.replace("\\", "/")
 def main():
     i = 0
-    path = "C:/Users/golam/" + pathinput +"/" #Directory of Path where files are located
+    path = pathinput +"/"   #Directory of Path where files are located
     for filename in os.listdir(path):
-        my_dest = "text" + str(i) + ".txt"
-        my_source = path + filename
-        my_dest = path + my_dest
-        os.rename(my_source, my_dest)
+        my_dest = "text" + str(i) + ".text"      #Name of files and their extensions, can be modified
+        my_source = path + filename             #Defining the location of source file for rename function
+        my_dest = path + my_dest                #Defining the location of destination file for rename function
+        os.rename(my_source, my_dest)           
         i += 1
 
 if __name__ == "__main__":
