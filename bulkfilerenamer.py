6a50d81bd79b971965f348f542a1e574c675e0c6
IKIGA-I <golamrahman268224@gmail.com>
2023-02-23 10:33:42 +0800

    Users can now input the filename and filetype as well, reducing the need for them to modify the code

--- bulkfilerenamer.py
+++ bulkfilerenamer.py
@@ -1,13 +1,16 @@
-import os #letting the program know what os we are using currently
+import os                                       #letting the program know what os we are using currently
 
-pathinputtemp = input('Input file path: ')
-                                                #Taking input of File path can be modified
-pathinput = pathinputtemp.replace("\\", "/")
+pathinputtemp = input('Input file path: ')      #Taking input of File path and storing in a temporary variable
+pathinput = pathinputtemp.replace("\\", "/")    #Making modifications, i.e correcting file path for use in the python function
+
+thefilename = input('Name of new Files: ')      #Taking input of File Name
+thefiletype = input("Please input what type of files they are(i.e extension): ")    #Taking input of File extension
+
 def main():
     i = 0
-    path = pathinput +"/"   #Directory of Path where files are located
+    path = pathinput +"/"                       #Directory of Path where files are located
     for filename in os.listdir(path):
-        my_dest = "text" + str(i) + ".text"      #Name of files and their extensions, can be modified
+        my_dest = thefilename + str(i) + "." + thefiletype     #Name of files and their extensions concatenated
         my_source = path + filename             #Defining the location of source file for rename function
         my_dest = path + my_dest                #Defining the location of destination file for rename function
         os.rename(my_source, my_dest)           
